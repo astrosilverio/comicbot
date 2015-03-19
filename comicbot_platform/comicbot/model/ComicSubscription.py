8246bd22a87ec2220930dfe774850215ba721a94
Katie Silverio <katie.silverio@venmo.com>
2015-03-19 18:02:28 -0400

    add a variant pref

--- comicbot_platform/comicbot/model/ComicSubscription.py
+++ comicbot_platform/comicbot/model/ComicSubscription.py
@@ -10,6 +10,14 @@ class ComicSubscription(models.Model):
         (TYPE_ONGOING, 'ongoing'),
         (TYPE_ONETIME, 'onetime')
     )
+    COVER_TYPE_REGULAR = 'RG'
+    COVER_TYPE_VARIANT = 'VR'
+    COVER_TYPE_BOTH = 'BT'
+    VARIANT_PREF_CHOICES = (
+        (COVER_TYPE_REGULAR, 'regular'),
+        (COVER_TYPE_VARIANT, 'variant'),
+        (COVER_TYPE_BOTH, 'both')
+    )
 
     comic = models.ForeignKey(Comic, related_name='subscriptions')
     issue = models.BooleanField(default=False)
@@ -18,3 +26,4 @@ class ComicSubscription(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     expire_date = models.DateTimeField(null=True)
     subscription_type = models.CharField(choices=SUBSCRIPTION_TYPE_CHOICES, max_length=2, default=TYPE_ONGOING)
+    variant_pref = models.CharField(choices=VARIANT_PREF_CHOICES, max_length=2, default=COVER_TYPE_BOTH)
