7fa5bed766be7bd7c1c2fe8a450dfaf0c78324c4
Katie Silverio <katie.silverio@venmo.com>
2015-03-18 22:45:55 -0400

    Fill out sub model

--- comicbot_platform/comicbot/model/ComicSubscription.py
+++ comicbot_platform/comicbot/model/ComicSubscription.py
@@ -4,8 +4,17 @@ from models.Comic import Comic
 
 class ComicSubscription(models.Model):
 
-    comic = models.ForeignKey(Comic, related_name='subscriptions')
-    issue = models.BooleanField('issue', default=False)
-    trade = models.BooleanField('trade', default=False)
-    hardcover = models.BooleanField('hardcover', default=False)
+    TYPE_ONGOING = 'GO'
+    TYPE_ONETIME = 'NT'
+    SUBSCRIPTION_TYPE_CHOICES = (
+        (TYPE_ONGOING, 'ongoing'),
+        (TYPE_ONETIME, 'onetime')
+    )
 
+    comic = models.ForeignKey(Comic, related_name='subscriptions')
+    issue = models.BooleanField(default=False)
+    trade = models.BooleanField(default=False)
+    hardcover = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    expire_date = models.DateTimeField(null=True)
+    subscription_type = models.CharField(choices=SUBSCRIPTION_TYPE_CHOICES, max_length=2, default=TYPE_ONGOING)
