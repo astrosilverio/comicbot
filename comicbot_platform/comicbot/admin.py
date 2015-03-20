264402d12b23d934186a1268073020618090ac42
Katie Silverio <katie.silverio@venmo.com>
2015-03-20 15:27:41 -0400

    admin

--- comicbot_platform/comicbot/admin.py
+++ comicbot_platform/comicbot/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from comicbot.models import Comic
+from comicbot.models import ComicSubscription
+from comicbot.models import User
+from comicbot.models import Release
+
+admin.site.register(Comic)
+admin.site.register(ComicSubscription)
+admin.site.register(User)
+admin.site.register(Release)
