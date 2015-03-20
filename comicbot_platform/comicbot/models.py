a7ceb742674374e8afdf6eea6969eb2b6165c880
Katie Silverio <katie.silverio@venmo.com>
2015-03-20 11:55:52 -0400

    help I have no idea what I'm doing

--- comicbot_platform/comicbot/models.py
+++ comicbot_platform/comicbot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from model.User import User
+from model.ComicSubscription import ComicSubscription
+from model.Comic import Comic
+from model.Release import Release
+
 # Create your models here.
